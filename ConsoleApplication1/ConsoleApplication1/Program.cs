750e307228fb44508c8c7e9ca424c9450bf6ce24
philong <hoangphihong1422000@gmail.com>
2019-10-01 14:36:30 +0700

    Update 1.10.2019

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -22,5 +22,35 @@ namespace ConsoleApplication1
         {
             return true;
         }
+
+
+        //input : float x
+        //output : a int that smallest but not smaller than X
+        //idea : first change float x int o integer then ++1
+        //      if x is a negative number than --1
+        //      if x == n then return x
+        public static int Ceil(float x)
+        {
+            int n = (int)x;
+            if (x != 0)
+            if(x > 0)
+                n = n + 1;   
+            return n;
+
+        }
+
+        
+        public static int FLoor(double x)
+        {
+            int n = (int)x;
+            if (x < 0)
+            {
+                if ( n != x)
+                {
+                    n = n - 1;
+                }
+            }       
+            return n;
+        }
     }
 }
